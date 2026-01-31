6cbc82d1d41d6134126a7be2f5fec7d9169e755c
Benzyl-titanium <benzyl.titanium@gmail.com>
2026-01-31 17:37:09 +0800

    Feat: use marvinjs pro

--- public/marvin/js/webservices.js
+++ public/marvin/js/webservices.js
@@ -9,13 +9,13 @@ function getDefaultServices() {
     };
     var services = {
         "clean2dws":         buildUrl("clean2d"),
-        // "clean3dws":         buildUrl("clean3d"),
+        "clean3dws":         buildUrl("clean3d"),
         "molconvertws":      buildUrl("molExport"),
         "stereoinfows":      buildUrl("stereoInfo"), // Calculate stereo
         // "reactionconvertws": buildUrl("reactionExport"),
         "hydrogenizews":     buildUrl("hydrogenizer"), // Add/Remove explicit H
-        // "automapperws":      buildUrl("automapper"), // Auto Map
-        // "aromatizews":       buildUrl("aromatizer") // Aromatize/dearomatize
+        "automapperws":      buildUrl("automapper"), // Auto Map
+        "aromatizews":       buildUrl("aromatizer") // Aromatize/dearomatize
     };
     return services;
 }
